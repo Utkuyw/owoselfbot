2d96cd5eadd74a347917cb2e8e23f1468c5b9926
Utkuyw <75106611+Utkuyw@users.noreply.github.com>
2022-01-09 22:51:32 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  Token: "", 
-  Prefix: "!",
-  BotOwner: "", //kendi hesap idnizi yazın
-  OwoBotID: "408785106942164992", // Varsayılan Değiştirmeyin!
-  UserID: [], // Bu kısıma arkadaşlarınızın ID'sini yazabilirsiniz bot spam'a düşmesin diye yapıldı. arada bir ıdsi girilen kişilere etiket atar.
-  GuildID: "" //botun mesaj atacağı sunucu id yaz
-};
